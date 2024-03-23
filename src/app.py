d783d0026b4ba21fc4c0440498c761d577863a0a
silvy8a <157405334+silvy8a@users.noreply.github.com>
2024-03-23 09:28:51 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -1,15 +1,47 @@
-import os
-from sqlalchemy import create_engine
-import pandas as pd
 from dotenv import load_dotenv
-
-# load the .env file variables
 load_dotenv()
+import os
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+import spotipy
+from spotipy import SpotifyClientCredentials
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+#Connection
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+sp=spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id, client_secret))
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+#artist_uri = 'spotify:artist:503mwh1GWEiWy9bzzpiTFW'
+results = sp.artist_top_tracks("503mwh1GWEiWy9bzzpiTFW")
+tracks=results['tracks']
+#print(tracks)
+
+#Getting the name, popularity and duration (conversion included)
+filtered_tracks=[]     
+for track in tracks:
+    filtered_track = {}
+    for k, v in track.items():
+        if k in ["duration_ms"]:
+            filtered_track[k] = (v / (1000*60)) %60
+        elif k in ["name", "popularity"]:
+            filtered_track[k] = v
+        else:
+            continue
+    filtered_tracks.append(filtered_track)
+#print(filtered_tracks)
+
+#Converting the results into a dataframe
+af_tracks=pd.DataFrame.from_dict(filtered_tracks)
+
+#Sorting the tracks to get the top 3
+af_tracks.sort_values(by=['popularity'], ascending=False).head(3)
+
+#Plotting to look for a possible correlation between the duration of the song and its popularity
+plt.scatter(af_tracks['popularity'], af_tracks['duration_ms'], )
+plt.xlabel("Song popularity")
+plt.ylabel("Song duration in min")
+plt.show()
+
+#As shown, there is no correlation between the duration of the song and its popularity.
